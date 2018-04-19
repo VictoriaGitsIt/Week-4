63d62e160b9a6155ca48e11712ec9b71e6ce9c41
Victoria Latimer <victorialatimer@Victorias-MacBook-Pro.local>
2018-04-19 11:29:57 -0500

    initial commit.

---
+++ fixed.js
@@ -0,0 +1,4 @@
+$('colorsBtn').click(function(){
+	
+	
+}
---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('#colorsBtn').click(function(){
+	$.getJSON('colors.json',function(result){
+		for(var i = 0; i < result.colors.length; i++ ){
+			$('#swatchPanel').append('<div style="background-color:'+ result.colors[i].code.hex +'" class="colorSwatch">' + result.colors[i].color.toUpperCase() + '<div class="cat">'+result.colors[i].category+'</div>' + '</div');
+		}
+	});
+});
